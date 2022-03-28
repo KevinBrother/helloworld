52174475c7a8fc002d1674129e8d31dd851c0441
caojunjie <1301239018@qq.com>
2022-03-28 10:43:16 +0800

    react-router-v5

--- react-router-v5/src/App.js
+++ react-router-v5/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import './index.css';
