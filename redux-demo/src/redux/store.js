8ed975809aa7291dc6f17651a3f0c0288a06d109
caojunjie <1301239018@qq.com>
2022-03-30 17:59:45 +0800

    使用redux插件

--- redux-demo/src/redux/store.js
+++ redux-demo/src/redux/store.js
@@ -2,9 +2,11 @@ import { createStore } from 'redux';
 import countReducer from './reducers/count';
 import personReducer from './reducers/person';
 import { combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 let reducers = combineReducers({
   count: countReducer,
   person: personReducer
 });
 
-export default createStore(reducers);
+export default createStore(reducers, composeWithDevTools());
