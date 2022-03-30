04eb085662eb471b764af80050dd3ed739702c74
caojunjie <1301239018@qq.com>
2022-03-30 16:27:15 +0800

    目录结构优化

--- redux-demo/src/containers/count.js
+++ redux-demo/src/containers/count.js
@@ -1,25 +1,57 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CountUi from '../Count';
 import {
   createIncreaseAction,
   createDecreaseAction
-} from '../action/countAction';
+} from '../redux/actions/count';
 
-/* function mapStateToProps(state) {
-  return { count: state };
-}
+class Count extends Component {
+  increase = () => {
+    const { value } = this.selectNum;
 
-function mapDispatchToProps(dispatch) {
-  return {
-    increase: (value) => dispatch(createIncreaseAction(value)),
-    decrease: (value) => dispatch(createDecreaseAction(value))
+    this.props.increase(value);
   };
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(CountUi);
- */
+  decreases = () => {
+    const { value } = this.selectNum;
+    this.props.decrease(value);
+  };
+
+  increaseIfOdd = () => {
+    const { value } = this.selectNum;
+    if (this.props.count % 2 !== 0) {
+      this.props.increase(value);
+    }
+  };
+
+  increaseAsync = () => {
+    const { value } = this.selectNum;
+    setTimeout(() => {
+      this.props.increase(value);
+    }, 500);
+  };
+
+  render() {
+    console.log('##', this.props);
+    return (
+      <div>
+        <h1>结果为 {this.props.count}</h1>
+        <select ref={(c) => (this.selectNum = c)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+
+        <button onClick={this.increase}>+</button>
+        <button onClick={this.decreases}>-</button>
+        <button onClick={this.increaseIfOdd}>奇数加</button>
+        <button onClick={this.increaseAsync}>异步的加</button>
+      </div>
+    );
+  }
+}
 
 export default connect((state) => ({ count: state }), {
   increase: createIncreaseAction,
   decrease: createDecreaseAction
-})(CountUi);
+})(Count);
