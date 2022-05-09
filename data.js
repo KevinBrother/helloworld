b4cf1a570dc34d3f964818e94ea0283ea01fdce4
caojunjie <1301239018@qq.com>
2022-05-09 21:53:09 +0800

    echart基本使用，工时统计饼图案例

---
+++ data.js
@@ -0,0 +1,55 @@
+var data = [
+  { value: 129, name: '中原银行2期开发' },
+  { value: 52, name: '工时统计' },
+  { value: 116.5, name: 'studio' },
+  { value: 49, name: 'RPA文档历史版本功能' },
+  { value: 56, name: 'robot' },
+  { value: 122.5, name: 'DP-RPA' },
+  { value: 175, name: '财务审核系统' },
+  { value: 165, name: 'console' },
+  { value: 59, name: '其他' },
+  { value: 97.5, name: '福建农信' }
+];
+
+var studio = [
+  {
+    value: 52,
+    name: '熟悉studio的代码'
+  },
+  {
+    value: 0, // TODO 用总共的减去52
+    name: 'bug与feature'
+  },
+  {
+    value: 6,
+    name: '对提示进行优化，当用户权限不足时，进行任务的发布，提示账户没有权限'
+  },
+  {}
+];
+
+var robot = [
+  {
+    value: 2,
+    name: '中债登的登录bug'
+  },
+  {
+    value: 20,
+    name: 'robot 开发模式重构，提高 热更新速度'
+  },
+  {
+    value: 4,
+    name: '许可证显示二维码'
+  },
+  {
+    value: 16,
+    name: '【studio & robot】日志模块优化'
+  },
+  {
+    value: 6,
+    name: 'robot与studio的logo替换改为配置的方式'
+  },
+  {
+    value: 8,
+    name: '升级 robot electron 的版本和studio保持一致,处理兼容性的bug'
+  }
+];
