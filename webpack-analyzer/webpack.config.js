6056220786dabd33281f03ee44da6842fa33ede7
caojunjie <1301239018@qq.com>
2022-05-10 18:53:54 +0800

    代码分析案例

---
+++ webpack-analyzer/webpack.config.js
@@ -0,0 +1,33 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+
+const smp = new SpeedMeasurePlugin();
+
+module.exports = smp.wrap({
+  mode: 'production',
+  entry: resolve(__dirname, './src/index.tsx'),
+  output: {
+    clean: true
+  },
+  module: {
+    rules: [
+      { test: /\.(ts|tsx)$/, use: 'babel-loader' },
+      { test: /\.txt$/, use: 'raw-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: resolve(__dirname, './index.html') }),
+    new BundleAnalyzerPlugin()
+  ]
+});
