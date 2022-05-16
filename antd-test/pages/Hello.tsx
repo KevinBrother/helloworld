d695cab297e5ac7122bb13b85fe364e5b3ef9e33
caojunjie <1301239018@qq.com>
2022-05-16 21:48:42 +0800

    排除的优化操作

--- antd-test/pages/Hello.tsx
+++ antd-test/pages/Hello.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Button } from 'antd'
+import './index.less'
 
 export default function Hello() {
   return (
-    <div><Button>hello world</Button></div>
+    <div className='div-border'><Button>hello world22212</Button></div>
   )
 }
--- antd-test/webpack.config.js
+++ antd-test/webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
     rules: [
       {
         test: /\.js|.ts|tsx$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -39,9 +40,9 @@ module.exports = {
             }
           }
         ]
-      },
-      { test: /\.(png|jpg|gif|svg)$/, use: ['file-loader'] },
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+      }
+      // { test: /\.(png|jpg|gif|svg)$/, use: ['file-loader'] },
+      // { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
     ]
   },
   resolve: {
