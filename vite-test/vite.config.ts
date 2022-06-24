34c9b550f02b02bd3955994a244455862f3767fa
caojunjie <1301239018@qq.com>
2022-06-24 13:25:16 +0800

    安装vite-plugin-svgr

--- vite-test/vite.config.ts
+++ vite-test/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
@@ -9,5 +10,5 @@ export default defineConfig({
       App: path.resolve(__dirname, 'src/App.tsx')
     }
   },
-  plugins: [react()]
+  plugins: [svgr(), react()]
 })
