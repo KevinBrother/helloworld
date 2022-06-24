eebc14574a69fad5daefe0fc057ea00f1945ef72
caojunjie <1301239018@qq.com>
2022-06-24 14:36:53 +0800

    vite+webpack的组合使用

---
+++ vite-webpack/README.md
@@ -0,0 +1,7 @@
+# vite+webpack
+
+- vite的开发版与webpack的生产环境版
+
+## TODO
+
+- vite与webpack的模板文件隔离，因为vite会依赖一个script脚步文件
---
+++ vite-webpack/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+console.log('[  ] >', path.resolve(__dirname, './index.html'));
+module.exports = {
+  mode: 'development',
+  entry: './src/main.tsx',
+  devServer: {
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.(ts|tsx)$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html')
+    })
+  ]
+};
