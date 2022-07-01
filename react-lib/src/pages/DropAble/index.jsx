716a7dab1c9d2d8ea0e0d4b254cc7965620b9b62
caojunjie <1301239018@qq.com>
2022-07-01 14:05:35 +0800

    拖拽样式

--- react-lib/src/pages/DropAble/index.jsx
+++ react-lib/src/pages/DropAble/index.jsx
@@ -12,15 +12,11 @@ const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
-
   return result;
 };
 
-const grid = 8;
-const getListStyle = (isDraggingOver) => ({
-  background: isDraggingOver ? 'lightblue' : 'lightgrey',
-  padding: grid,
-  width: 250
+const getItemStyle = (isDragging, draggableStyle) => ({
+  ...draggableStyle
 });
 
 export default function DropAble() {
@@ -41,41 +37,47 @@ export default function DropAble() {
   }
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div className="droppable-header">
-        <div>标签</div>
-        <div>operations</div>
-      </div>
-      <Droppable droppableId="all-droppables">
-        {(provided, snapshot) => (
-          <div
-            className="droppable-wrap"
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided, snapshot) => (
-                  <div
-                    className="draggable-item"
-                    {...provided.draggableProps}
-                    ref={provided.innerRef}
-                  >
-                    <div>
-                      {item.content} {item.id}
+    <div style={{ width: '500px' }}>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="droppable-header">
+          <div>标签</div>
+          <div>operations</div>
+        </div>
+        <Droppable droppableId="tag-droppable">
+          {(provided, snapshot) => (
+            <div
+              className="droppable-wrap"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      className="draggable-item"
+                      {...provided.draggableProps}
+                      ref={provided.innerRef}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      <div className="tag-content">
+                        {item.content} {item.id}
+                      </div>
+                      <div>
+                        first
+                        <div {...provided.dragHandleProps}>drag1</div>
+                        end
+                      </div>
                     </div>
-                    <div>
-                      first
-                      <div {...provided.dragHandleProps}>drag1</div>
-                      end
-                    </div>
-                  </div>
-                )}
-              </Draggable>
-            ))}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
   );
 }
