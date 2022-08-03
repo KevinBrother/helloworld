208cc7990f97ba8677a2153102f21b5fc4cc5249
caojunjie <1301239018@qq.com>
2022-08-03 16:11:39 +0800

    fix(svg2RIcon) 添加测试文件

--- code-collet/test/svg2RIcon.test.ts
+++ code-collet/test/svg2RIcon.test.ts
@@ -1,15 +1,20 @@
 import {
-  underlineToHump,
   getFilenamesByPathAndSuffix,
   genImportGrammarByFileNames,
   genIconRFCByFileName,
   genTSXFile
 } from '../src/svg2RIcon'
+import { underlineToHump } from '../src/utils/index';
 
 test('a_ad_qe_dsa to upperCase', () => {
   expect(underlineToHump('a_ad_qe_dsa')).toEqual('AAdQeDsa');
 })
 
 test('获取所有后缀为json的文件名', () => {
+  // TODO 这么写也是过的，有问题
+
   const filenames = getFilenamesByPathAndSuffix('./', '.json');
+  filenames.forEach(filename => {
+    expect(filename.includes('.js')).toBeTruthy();
+  })
 })
