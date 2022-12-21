958fdf97fee55a131d8e6abfa61ea7593f81b972
caojunjie <1301239018@qq.com>
2022-12-21 14:47:24 +0800

    feat(webpack) federation lib-demo

---
+++ webpack-module-federation/packages/lib-demo/webpack.config.js
@@ -0,0 +1,20 @@
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  devtool: 'hidden-source-map',
+  output: {
+    clean: true
+  },
+  module: {},
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'lib_demo',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './add': './src/add.js'
+      }
+    })
+  ]
+};
--- webpack-module-federation/packages/main-app/webpack.config.js
+++ webpack-module-federation/packages/main-app/webpack.config.js
@@ -41,7 +41,8 @@ module.exports = {
       },
       remotes: {
         // 'component-app': 'component_app@http://localhost:8082/remoteEntry.js'
-        'component-app': 'componentApp@http://localhost:8082/remoteEntry.js'
+        'component-app': 'componentApp@http://localhost:8082/remoteEntry.js',
+        'lib-demo': 'lib_demo@http://localhost:8083/remoteEntry.js'
       },
       shared: {
         ...dependencies,
