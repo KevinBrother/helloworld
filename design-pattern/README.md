8f0574a0811a63674d69defa08a38f07ed2097c5
caojunjie <1301239018@qq.com>
2023-01-17 17:34:46 +0800

    feat(design-pattern) build脚本修改

--- design-pattern/README.md
+++ design-pattern/README.md
@@ -2,15 +2,13 @@
 
 ## 命令
 
-```bash
- 
+``` bash 
 npm i -g zx
-yarn go
-
 ```
 
-注意修改tsconfig.json的rootDirs
-
+```bash
+yarn go --tag=builder
+```
 ## doc
 
 [菜鸟教程](https://www.runoob.com/design-pattern/design-pattern-tutorial.html)
---
+++ design-pattern/build.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env zx
+
+const { tag } = argv;
+
+const distMap = {
+  builder: '建造者模式'
+};
+
+const distValue = distMap[tag];
+
+const tsconfigPath = './tsconfig.json';
+const tsconfig = await fs.readJson(tsconfigPath);
+
+tsconfig.include = [`./src/${distValue}`];
+await fs.writeJson(tsconfigPath, tsconfig, { spaces: 2 });
+await $`rm -rf dist && tsc && node ./dist/index.js`;
