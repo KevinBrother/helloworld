dd1ef426e397a279eb3ebdbc394ebcd44116e5e3
caojunjie <1301239018@qq.com>
2023-02-08 17:14:58 +0800

    feat(node-study) event-emitter

---
+++ node-study/node-core/event-emitter/01.base.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+const event = new EventEmitter();
+
+event.on('some_event', function () {
+  console.log('some_event 事件触发');
+});
+setTimeout(function () {
+  event.emit('some_event');
+}, 1000);
