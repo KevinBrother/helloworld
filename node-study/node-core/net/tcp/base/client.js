4d042e7659489ced47bf8a7c05672a26924a6bcd
caojunjie <1301239018@qq.com>
2023-02-08 16:08:27 +0800

    feat(node-study) net

--- node-study/node-core/net/tcp/base/client.js
+++ node-study/node-core/net/tcp/base/client.js
@@ -1,15 +1,11 @@
 const net = require('net');
 const HOST = '127.0.0.1';
 const PORT = 3000;
-
+process.title = 'net tcp client';
+console.log('client :', process.pid);
 // 创建连接客户端
 const client = net.createConnection(PORT, HOST, () => {
   console.log('客户端连接成功 日志1');
-  console.log(
-    '%c [ client ]-7',
-    'font-size:13px; background:pink; color:#bf2c9f;',
-    client
-  );
 });
 
 // 监听连接后开始发送数据
@@ -31,11 +27,7 @@ client.on('data', (buffer) => {
 });
 
 client.on('error', (err) => {
-  console.error(
-    '%c [ err ]-29',
-    'font-size:13px; background:pink; color:#bf2c9f;',
-    err
-  );
+  console.error('%c [ err ]-29', 'font-size:13px; background:pink; color:#bf2c9f;', err);
 });
 
 client.on('close', (err) => {
