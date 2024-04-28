466d439fe0a7d9b014b8c435a4b42959ec4c4520
caojunjie <1301239018@qq.com>
2024-04-28 14:58:55 +0800

    feat(util-static-server)

--- node-utils/packages/static-server/index.ts
+++ node-utils/packages/static-server/index.ts
@@ -2,17 +2,47 @@ import http from 'http';
 import path from 'path';
 import fs from 'fs';
 
+const mines = {
+  img: 'image/png',
+  js: 'text/javascript',
+  css: 'text/css',
+  html: 'text/html'
+}
+
 const server = http.createServer((req, res) => {
+  if(req.method !== 'GET') {
+    res.writeHead(405, { 'Content-Type': 'text/html' });
+    res.end('<h1> 405 Method Not Allowed </h1>');
+    return;
+  }
+ 
   const dirName = 'static';
   const filePath = path.join(__dirname, dirName, req.url!);
 
   fs.readFile(filePath, (err, data) => {
     if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not found');
-        return;
+      res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+      switch (err.code) {
+        case 'ENOENT':
+          res.statusCode = 404;
+          res.end('<h1>404 Not found</h1>');
+          break;
+        case 'EISDIR':
+          res.statusCode = 403;
+          res.end('<h1>403 Forbidden</h1>');
+          break
+        default:
+          res.statusCode = 500;
+          res.end('<h1>500 Internal Server Error</h1>');
+          break;
+      }
+      return;
     }
 
+    const ext = path.extname(filePath).slice(1);
+    const contentType = mines[ext] || 'text/plain';
+    // text/html; charset=UTF-8
+    res.writeHead(200, { 'Content-Type': contentType + '; charset=UTF-8'});
     res.end(data);
   })
 })
--- node-utils/packages/static-server/static/index.js
+++ node-utils/packages/static-server/static/index.js
@@ -1,3 +1,4 @@
+// 点击弹框效果
 document.querySelector('div').addEventListener('click', function() {
   alert('Hello, World!');
 });
