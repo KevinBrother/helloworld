b482d8af1cf1c355ffa4bb010384836f8f37248b
caojunjie <1301239018@qq.com>
2024-06-12 16:41:04 +0800

    feat(carousel): 轮播图基础结构

---
+++ jquery-case/carousel/index.js
@@ -0,0 +1,31 @@
+
+// 切换广告图片
+function imgChange() {
+    // 补全功能
+}
+
+// 切换对应原点
+function dotChange() {
+
+}
+
+// 自定义 动画 完成广告图自动切换功能，动画完成时间为 400ms
+function autoChange() {
+
+}
+// 鼠标放入停止自动播放，且展示左右箭头。 鼠标移出开始自动播放，且隐藏左右箭头。
+function stopChange() {
+    
+}
+
+// 点击左箭头功能，切换到上一张广告图。动画时间 400ms。
+$('.prev').click(function() {
+    
+
+})
+
+// 点击右箭头功能，切换到下一张广告图。动画时间 400ms。
+
+$('.next').click(function() {
+
+})
