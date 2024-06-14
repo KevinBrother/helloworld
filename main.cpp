42b1a9ccc2671ce9a8fe2a650b8c085ef4da9ab9
caojunjie <1301239018@qq.com>
2024-06-14 22:35:56 +0800

    feat(单个 cpp 的编译和调试)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+int swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    int a = 10, b = 20;
+    std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+    swap(a, b);
+    std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
     return 0;
 }
