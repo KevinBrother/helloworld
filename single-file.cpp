ebc227d2c8224d7afd81b4a7b49ccead7380016d
caojunjie <1301239018@qq.com>
2024-06-17 11:16:13 +0800

    test

--- single-file.cpp
+++ single-file.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 void swap(int &a, int &b) {
     int temp = a;
@@ -7,6 +9,7 @@ void swap(int &a, int &b) {
 }
 
 int main() {
+    vector<int> result = {2,7,11,15, 100};
     int a = 10, b = 20;
     std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
     swap(a, b);
