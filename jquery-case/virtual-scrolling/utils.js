a1e408d814f2b347135802d81376dc7bd0119037
caojunjie <1301239018@qq.com>
2024-06-19 13:39:17 +0800

    feat(virtual-scroll)

---
+++ jquery-case/virtual-scrolling/utils.js
@@ -0,0 +1,9 @@
+function mockData(count = 1000) {
+  const data = [];
+  for (let i = 0; i < count; i++) {
+    this.data.push({
+      text: `${this.data.length} --- ${Math.random().toString(36)}`,
+    });
+  }
+  return data;
+}
