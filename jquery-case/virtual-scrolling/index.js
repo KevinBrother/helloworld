2c68e6296c86e35d9364ed391c0c122eb738e505
caojunjie <1301239018@qq.com>
2024-06-20 15:20:08 +0800

    feat(vs-scroll): 计算与联动相关

--- jquery-case/virtual-scrolling/index.js
+++ jquery-case/virtual-scrolling/index.js
@@ -4,6 +4,8 @@ import { mockData } from "./utils.js";
 
 class VirtualScrolling {
   data = [];
+  containerTopPadding = 20;
+  scrollBarHeight = 20;
 
   constructor({
     data,
@@ -12,8 +14,9 @@ class VirtualScrolling {
     pageSize,
     itemHeight,
     containerHeight,
-    scrollContainer,
-    scrollBar,
+    vsMain,
+    vsScrollBar,
+    vsScroll,
   }) {
     this.data = data;
     this.container = container;
@@ -21,8 +24,13 @@ class VirtualScrolling {
     this.pageSize = pageSize;
     this.itemHeight = itemHeight;
     this.containerHeight = containerHeight;
-    this.scrollContainer = scrollContainer;
-    this.scrollBar = scrollBar;
+    this.vsMain = vsMain;
+    this.vsScrollBar = vsScrollBar;
+    this.vsScroll = vsScroll;
+    this.vsMainTotalHeight = data.length * itemHeight;
+
+    this.realContainerHeight =
+      this.vsMainTotalHeight + 20 + this.containerTopPadding - containerHeight;
 
     this.bindEvent();
     this.render();
@@ -30,83 +38,122 @@ class VirtualScrolling {
 
   bindEvent() {
     // 内存区域滚动事件
-    this.scrollContainer.on("mousewheel", this.handleContainer);
+    this.vsMain.on("mousewheel", this.handleVSMain);
 
     // 滚动条点击事件
-    this.scrollBar.on("click", this.handleClickScrollBar);
+    this.vsScroll.on("click", this.handleClickVSScroll);
 
-    // 滚动条拖动事件
-    this.scrollBar.on("mousedown", (e) => {
-      const startY = e.clientY;
-      const startScrollTop = this.scrollContainer.scrollTop();
-      const startPercent =
-        (startScrollTop /
-          (this.scrollContainer.prop("scrollHeight") -
-            this.scrollContainer.prop("clientHeight"))) *
-        100;
+    this.vsScrollBar.on("dragstart", (event) => {
+      // 隐藏默认拖动的图片样式
+      var transparentImage = new Image();
+      transparentImage.src =
+        "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+      event.originalEvent.dataTransfer.setDragImage(transparentImage, 0, 0);
+    });
 
-      $(document).on("mousemove", (e) => {
-        const endY = e.clientY;
-        const deltaY = endY - startY;
-        const percent = startPercent + (deltaY / this.scrollBar.height()) * 100;
-        const scrollTop =
-          (this.scrollContainer.prop("scrollHeight") -
-            this.scrollContainer.prop("clientHeight")) *
-          (percent / 100);
-        this.scrollContainer.scrollTop(scrollTop);
-      });
+    this.vsScrollBar.on("click", (event) => {
+      // 阻止事件冒泡，否则会触发 vsScrollBar 的点击事件，但是offsetY 为 vsScrollBar 元素的值。
+      event.stopPropagation();
+    });
 
-      $(document).on("mouseup", () => {
-        $(document).off("mousemove");
-        $(document).off("mouseup");
-      });
+    this.vsScrollBar.on("drag", (event) => {
+      if (event.pageY === 0) {
+        return;
+      }
+      // console.log(event);
+      const $parent = $(event.target).parent();
+      // 可以通过 getBoundingClientRect 获取元素的位置信息
+      const { top } = $parent.offset();
+      // console.log(`parent offset top: ${top}, pageY: ${event.pageY}`);
+      const total = event.pageY - top;
+      if (total < 0) {
+        this.vsScrollBar.css("transform", `translateY(0px)`);
+        return;
+      }
+
+      if (total > this.containerHeight) {
+        this.vsScrollBar.css(
+          "transform",
+          `translateY(${this.containerHeight}px)`
+        );
+        return;
+      }
+
+      this.vsScrollBar.css("transform", `translateY(${total}px)`);
     });
   }
 
-  handleClickScrollBar = (e) => {
-    const scrollHeight = this.scrollContainer.prop("scrollHeight");
-    const clientHeight = this.scrollContainer.prop("clientHeight");
-    const percent = (e.offsetX / this.scrollBar.width()) * 100;
-    const scrollTop = (scrollHeight - clientHeight) * (percent / 100);
-    this.scrollContainer.scrollTop(scrollTop);
+  handleClickVSScroll = (e) => {
+    var position =
+      e.offsetY < 0
+        ? 0
+        : e.offsetY > this.containerHeight - this.scrollBarHeight
+        ? this.containerHeight - this.scrollBarHeight
+        : e.offsetY;
+    const percent = position / this.containerHeight;
+
+    const mainPosition = Math.floor(this.vsMainTotalHeight * percent);
+
+    console.log(
+      `position: ${position}, offsetY: ${e.offsetY},
+       this.containerHeight: ${this.containerHeight}, 
+       mainPosition: ${mainPosition}, 
+       this.vsMainTotalHeight: ${this.vsMainTotalHeight}`
+    );
+    this.vsScrollBar.css("transform", `translateY(${position}px)`);
+    this.vsMain.css("transform", `translateY(-${mainPosition}px)`);
   };
 
-  handleContainer = (e) => {
-    const scrollHeight = this.scrollContainer.prop("scrollHeight");
-    const scrollTop = this.scrollContainer.scrollTop();
-    const clientHeight = this.scrollContainer.prop("clientHeight");
-    const translateY = this.scrollContainer
+  handleVSMain = (e) => {
+    const translateY = this.vsMain
       .css("transform")
       .match(/matrix\(\d+, \d+, \d+, \d+, \d+, (-?\d+)\)/)[1];
 
     const total = parseInt(e.originalEvent.wheelDelta) + parseInt(translateY);
-    console.log(total);
     if (total > 0) {
-      this.scrollContainer.css("transform", `translateY(0)`);
+      this.vsMain.css("transform", `translateY(0)`);
       return;
     }
 
-    this.scrollContainer.css("transform", `translateY(${total}px)`);
-    const percent = (scrollTop / (scrollHeight - clientHeight)) * 100;
-    this.scrollBar.css("width", percent + "%");
+    if (total < -this.realContainerHeight) {
+      this.vsMain.css(
+        "transform",
+        `translateY(${-this.realContainerHeight}px)`
+      );
+      return;
+    }
+
+    this.vsMain.css("transform", `translateY(${total}px)`);
+    const percent = Math.abs(total) / this.vsMainTotalHeight;
+    const scrollBarOffset = this.containerHeight * percent;
+    console.log(
+      "🚀 ~ VirtualScrolling ~ percent:",
+      percent,
+      total,
+      this.vsMainTotalHeight,
+      scrollBarOffset
+    );
+
+    this.vsScrollBar.css("transform", `translateY(${scrollBarOffset}px)`);
   };
 
   render() {
     const items = this.data.map((item) => {
       return $(this.template).text(item.text);
     });
-    this.scrollContainer.append(items);
+    this.vsMain.append(items);
   }
 }
 
 //
 new VirtualScrolling({
-  data: mockData(),
+  data: mockData(50),
   container: $(".container"),
-  scrollContainer: $(".scroll-container"),
-  scrollBar: $(".vs_scroll_bar-bar"),
+  vsMain: $(".vs_main"),
+  vsScrollBar: $(".vs_scroll_bar"),
+  vsScroll: $(".vs_scroll"),
   template: '<div class="inner"></div>',
   pageSize: 10,
-  itemHeight: 30, // TODO 应该可以优化掉
-  containerHeight: 500, // TODO 应该可以优化掉
+  itemHeight: 100 + 10, // TODO 应该可以优化掉
+  containerHeight: 520, // TODO 应该可以优化掉
 });
