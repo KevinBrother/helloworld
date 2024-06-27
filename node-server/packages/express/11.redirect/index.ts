b888171865940e7ede3cfd3f48dc224230358768
caojunjie <1301239018@qq.com>
2024-06-27 15:34:17 +0800

    feat(redirect): test done

---
+++ node-server/packages/express/11.redirect/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.cookie("name", "John Doe");
+  res.send("Hello World!");
+});
+
+app.get("/redirect", (req, res) => {
+  res.redirect("http://127.0.0.1:5500/node-server/packages/express/11.redirect/#/asdfasdf?code=121323");
+});
+
+
+app.listen(13000, () => {
+  console.log("Server is running on port 3000, visit http://localhost:13000/");
+});
