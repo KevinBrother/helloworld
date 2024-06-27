305fe39298a7ae45b1d5b5f99182cf74526d2979
caojunjie <1301239018@qq.com>
2024-06-27 16:32:06 +0800

    feat(redirect): test done

--- node-server/packages/express/11.redirect/index.ts
+++ node-server/packages/express/11.redirect/index.ts
@@ -10,9 +10,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/redirect", (req, res) => {
-  res.redirect("http://127.0.0.1:5500/node-server/packages/express/11.redirect/#/asdfasdf?code=121323");
+  res.redirect("http://127.0.0.1:13000/redirect-second?red_url=http://127.0.0.1:5500/node-server/packages/express/11.redirect/#/asdfasdf");
 });
 
+
+app.get("/redirect-second", (req, res) => {
+  res.redirect("http://127.0.0.1:5500/node-server/packages/express/11.redirect/#/asdfasdf?code=121323");
+});
 
 app.listen(13000, () => {
   console.log("Server is running on port 3000, visit http://localhost:13000/");
