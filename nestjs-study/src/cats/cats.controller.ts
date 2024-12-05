e2c06f3f6e19813db65d4bdc6e47a3cb71c7e972
caojunjie <1301239018@qq.com>
2024-12-05 19:16:45 +0800

    feat(guid): cats controller case

--- nestjs-study/src/cats/cats.controller.ts
+++ nestjs-study/src/cats/cats.controller.ts
@@ -1,11 +1,44 @@
-import { Controller, Get, Ip, Req } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Ip,
+  Param,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatDto } from './dto/list-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post('create')
+  create(@Body() createCatDto: CreateCatDto, @Res() res: Response) {
+    console.log(`this action adds a new cat ${createCatDto.name}`);
+    res.status(HttpStatus.CREATED).send('ok');
+  }
+
   @Get()
-  findAll(@Req() request: Request, @Ip() ip: string) {
-    console.log(request.url);
+  findAll(@Res() res: Response<CatDto[]>) {
+    res.status(HttpStatus.OK).send([{ name: 'cat1', age: 1, breed: 'breed1' }]);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string, @Res() res: Response<CatDto>) {
+    res.status(HttpStatus.OK).send({ name: 'cat1', age: 1, breed: 'breed1' });
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `this action deletes a #${id} cat`;
+  }
+
+  @Get('ab*cd')
+  test(@Req() request: Request, @Ip() ip: string) {
     console.log(ip, request.ip);
     return 'This action returns all cats';
   }
