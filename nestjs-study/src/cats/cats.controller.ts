ab4f09648bc4fcc2638e335df877e1b5f8cc6e73
caojunjie <1301239018@qq.com>
2024-12-07 23:21:19 +0800

    feat(module)

--- nestjs-study/src/cats/cats.controller.ts
+++ nestjs-study/src/cats/cats.controller.ts
@@ -12,12 +12,17 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
 import { CatDto } from './dto/list-cat.dto';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post('create')
   create(@Body() createCatDto: CreateCatDto, @Res() res: Response) {
+    this.catsService.create(createCatDto);
     console.log(`this action adds a new cat ${createCatDto.name}`);
     res.status(HttpStatus.CREATED).send('ok');
   }
@@ -28,7 +33,7 @@ export class CatsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string, @Res() res: Response<CatDto>) {
+  findOne(@Param('id') id: string, @Res() res: Response<Cat>) {
     res.status(HttpStatus.OK).send({ name: 'cat1', age: 1, breed: 'breed1' });
   }
 
