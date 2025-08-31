d42e82be0fcdd96c4d38922f52549d868c11f44f
Kevinbrother <1301239018@qq.com>
2025-08-31 22:12:59 +0800

    'playwright-api'

---
+++ playwirght-api/src/index.ts
@@ -0,0 +1,13 @@
+import { chromium, firefox, webkit } from 'playwright';
+
+(async () => {
+  const browser = await chromium.launch();  // Or 'firefox' or 'webkit'.
+  const page = await browser.newPage();
+  const rsp  = await page.goto('http://example.com');
+  // console.log('rsp', rsp)
+  const body = await rsp?.body()
+  console.log('resp.body()', body.toString())
+
+  // other actions...
+  await browser.close();
+})();
